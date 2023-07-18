65c3b3ed842381ab156df986164a782906ed44df
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 09:28:34 +0100

    created file exercises/3a073271-4a5c-41d1-bb0e-b9494b50cf83/skeletons/8f088838-6bb0-4023-a967-0ee4e7ec6806/s.py

---
+++ exercises/3a073271-4a5c-41d1-bb0e-b9494b50cf83/skeletons/8f088838-6bb0-4023-a967-0ee4e7ec6806/s.py
@@ -0,0 +1,22 @@
+colors = {"Black": (0, 0, 0), "White": (255,255,255), "Red": (255, 0, 0),
+          "Green": (0,255, 0), "Blue": (0, 0,255), "Yellow": (255,255, 0),
+          "Cyan": (0,255,255), "Magenta": (255, 0,255)}
+
+def calc_dist(color, r, g, b):
+    dist_r = (colors[color][0] - r) ** 2
+    dist_g = (colors[color][1] - g) ** 2
+    dist_b = (colors[color][2] - b) ** 2
+    dist = (dist_r + dist_g + dist_b) ** 0.5
+    return dist    
+
+def closest_color(r, g, b):
+    best = "Black"
+    best_dist = {{gap}}
+    for cl in colors:
+        dist = {{gap}}
+        if dist < best_dist: {{gap}}
+    return best
+
+print (closest_color(255, 165, 0)) # orange
+print (closest_color(255,192,203)) # pink
+print (closest_color(0,128,128)) # teal
