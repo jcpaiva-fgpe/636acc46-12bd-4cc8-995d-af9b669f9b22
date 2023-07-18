b794d792388fa29c34505e4a9d48f3867feed370
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 09:24:51 +0100

    created file exercises/52289f53-acfd-4d88-a088-bc5338f89fe8/skeletons/54488b93-483e-4283-94bd-93df3036c2d4/s.py

---
+++ exercises/52289f53-acfd-4d88-a088-bc5338f89fe8/skeletons/54488b93-483e-4283-94bd-93df3036c2d4/s.py
@@ -0,0 +1,12 @@
+card_strength = "A K Q J 10 9 8 7 6 5 4 3 2"
+
+def card_compare(card1, card2):
+    i1 = {{gap}}
+    i2 = {{gap}}
+    if i1 < i2: return 1
+    if i1 > i2: return 2
+    return 0
+
+print (card_compare("7", "A"))
+print (card_compare("Q", "9"))
+print (card_compare("10", "10"))
