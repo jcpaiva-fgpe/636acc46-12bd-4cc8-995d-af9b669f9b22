6d15dc955b292fb06159b177d71c68fce9ec4a29
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 08:50:45 +0100

    created file exercises/bb49b48c-be0c-4fc4-88aa-1d81c8b98292/skeletons/76246c76-471c-455c-b88b-994ab99f8ec6/s.py

---
+++ exercises/bb49b48c-be0c-4fc4-88aa-1d81c8b98292/skeletons/76246c76-471c-455c-b88b-994ab99f8ec6/s.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    left = None
+    right = None
+    def __init__(self, val):
+        self.val = val
+        
+    
+def find_bst(cur, val):
+    if not cur or val == cur.val:
+        return cur
+    if val < cur.val:
+        return {{gap}}
+    return {{gap}}
+
+root = TreeNode(18)
+root.left = TreeNode(7)
+root.right = TreeNode(27)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(15)
+print(find_bst(root, 4).val)
