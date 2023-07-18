e733ca25a80a2eb9f923cd7669b50c24037c7c50
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-18 09:05:10 +0100

    created file exercises/942dd76f-5f59-4263-a9f5-016627b86512/skeletons/4d84589b-27ee-4e94-884e-2ca6accf5c35/s.py

---
+++ exercises/942dd76f-5f59-4263-a9f5-016627b86512/skeletons/4d84589b-27ee-4e94-884e-2ca6accf5c35/s.py
@@ -0,0 +1,9 @@
+text = "I   don't  know   why    it   takes so  much     space.  "
+def text_clean(text):
+    while True:
+        l = {{gap}}
+        text = text.replace('  ', ' ')
+        if {{gap}}: break
+    return text
+        
+print(text_clean(text))
