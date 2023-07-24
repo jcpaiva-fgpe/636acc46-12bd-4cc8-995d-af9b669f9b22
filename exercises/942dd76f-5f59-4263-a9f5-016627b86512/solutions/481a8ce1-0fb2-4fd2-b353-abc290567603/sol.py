0fe29c2a4a2796e841b455a4d9a7a9b94a1de34e
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 20:27:31 +0100

    Removed exercises/942dd76f-5f59-4263-a9f5-016627b86512/solutions/481a8ce1-0fb2-4fd2-b353-abc290567603/sol.py

--- exercises/942dd76f-5f59-4263-a9f5-016627b86512/solutions/481a8ce1-0fb2-4fd2-b353-abc290567603/sol.py
+++
@@ -1,7 +0,0 @@
-text = "I   don't  know   why    it   takes so  much     space.  "
-def text_clean(text):
-    while True:
-        l = len(text)
-        text = text.replace('  ', ' ')
-        if l == len(text): break
-    return text
