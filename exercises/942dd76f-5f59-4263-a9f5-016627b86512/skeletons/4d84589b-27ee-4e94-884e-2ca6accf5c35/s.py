cec64e59658919d3efaa20cdf2d0c5e21c2af9fc
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-24 20:27:30 +0100

    Removed exercises/942dd76f-5f59-4263-a9f5-016627b86512/skeletons/4d84589b-27ee-4e94-884e-2ca6accf5c35/s.py

--- exercises/942dd76f-5f59-4263-a9f5-016627b86512/skeletons/4d84589b-27ee-4e94-884e-2ca6accf5c35/s.py
+++
@@ -1,9 +0,0 @@
-text = "I   don't  know   why    it   takes so  much     space.  "
-def text_clean(text):
-    while True:
-        l = {{gap}}
-        text = text.replace('  ', ' ')
-        if {{gap}}: break
-    return text
-        
-print(text_clean(text))
