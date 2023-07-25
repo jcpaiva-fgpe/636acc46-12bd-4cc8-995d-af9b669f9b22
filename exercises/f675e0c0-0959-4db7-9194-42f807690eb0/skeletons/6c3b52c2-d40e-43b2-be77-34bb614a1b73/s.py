e4a3b2e5dfd1b8b242ced24ceb16f56f4725dd09
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-25 09:29:14 +0100

    updated file exercises/f675e0c0-0959-4db7-9194-42f807690eb0/skeletons/6c3b52c2-d40e-43b2-be77-34bb614a1b73/s.py

--- exercises/f675e0c0-0959-4db7-9194-42f807690eb0/skeletons/6c3b52c2-d40e-43b2-be77-34bb614a1b73/s.py
+++ exercises/f675e0c0-0959-4db7-9194-42f807690eb0/skeletons/6c3b52c2-d40e-43b2-be77-34bb614a1b73/s.py
@@ -1,7 +1,7 @@
 card_strength = "A K Q J 10 9 8 7 6 5 4 3 2"
 
 def card_compare(card1, card2):
-    i1, i2 = card_strength.find(card1), card_strength.find(card2)
+    i1, i2 = {{gap}}
     if i1 < i2: return 1
     if i1 > i2: return 2
     return 0
