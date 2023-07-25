110b3326cc34c59f273d45352544715c656479d3
Thomas Naprawski <tnaprawski@gmail.com>
2023-07-25 09:20:52 +0100

    updated file exercises/319d205c-fc72-484a-94a6-83416eaf6c17/skeletons/1db0ca55-6194-421f-8f49-289bade81e28/s.py

--- exercises/319d205c-fc72-484a-94a6-83416eaf6c17/skeletons/1db0ca55-6194-421f-8f49-289bade81e28/s.py
+++ exercises/319d205c-fc72-484a-94a6-83416eaf6c17/skeletons/1db0ca55-6194-421f-8f49-289bade81e28/s.py
@@ -4,7 +4,7 @@ data = [ [ 1, 7, 2, None, None, 5],
 
 def impute(data):
     for i, row in enumerate(data):
-        r = [x for x in row if {{gap}}]
+        r = [x for x in row if x is not None]
         avg = sum(r) / len(r)
         data[i] = list(map(lambda x: {{gap}}, row))
         
